1bf539a6f1561f08fb265577d590b908d92438c4
Egor Komarov <egor.anakonda10@mail.ru>
2025-05-18 15:24:50 +0300

    Commit name

--- main1.py
+++ main1.py
@@ -9,6 +9,21 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+logger = logging.getLogger(__name__)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler("errors.log")
+file_handler.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
 
 def try_except_decorator(func):
     def wrapper(*args, **kwargs):
@@ -20,7 +35,7 @@ def try_except_decorator(func):
             logging.info(f'Функция выполнилась за {elapsed_time} секунд')
             return function
         except Exception as e:
-            logging.error(f"Ошибка в функции {func.__name__}: {e}")
+            logger.error(f"Ошибка в функции: {e}", exc_info=True)
             return None
     return wrapper
 
@@ -89,7 +104,7 @@ def parse_gymnasium_19(url):
             logging.info(f"{link}")
     else:
         logging.info("Не найдено.")
-
+    return 1/0
 
 if __name__ == "__main__":
     website_url = "https://orel-gym19.obr57.ru/"
