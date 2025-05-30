06797f0e7d1ee832aa671a6cb420b99487b1c20d
Egor Komarov <egor.anakonda10@mail.ru>
2025-05-30 10:36:20 +0300

    Commit name

--- main1.py
+++ main1.py
@@ -24,55 +24,55 @@ def parse_gymnasium_19(url: str) -> None:
     logging.info("\n Контактные данные:")
 
     # Телефон
-    def phone_numbers():
+    def find_phone_numbers():
         phone_link = soup.find('a', class_='departments__link', href=lambda x: x and x.startswith('tel:'))
         if phone_link:
             phone_number = phone_link.get_text(strip=True)
             return phone_number
-    logging.info(f'Номер телефона: {phone_numbers()}')
+    logging.info(f'Номер телефона: {find_phone_numbers()}')
 
     # Email
-    def email_address():
+    def find_email_address():
         email = soup.find('a', href=lambda x: x and x.startswith('mailto:'))
         if email:
             email_ = email.get_text(strip=True)
             return email_
-    logging.info(f'Email: {email_address()}')
+    logging.info(f'Email: {find_email_address()}')
 
     # Адрес
-    def address_school():
+    def find_address_school():
         address = soup.find('div', class_='contacts__text')
         if address:
             address_ur = address.get_text(strip=True)
             return address_ur[27:]
-    logging.info(f'Адрес: {address_school()}')
+    logging.info(f'Адрес: {find_address_school()}')
 
     # Директор
-    def director():
+    def find_director():
         director_div = soup.find('div', class_='user__name')
         if director_div:
             director_name = director_div.get_text(strip=True)
             raw_name = re.sub(r"(?<=\w)([А-ЯЁ])", r" \1", director_name)
             return raw_name
-    logging.info(f'Директор: {director()}')
+    logging.info(f'Директор: {find_director()}')
 
     # Завучи
     logging.info('\n Завучи:')
 
-    def _headteachers_():
+    def find_headteachers():
         all_lastnames_headteachers = ['/kop', '/evs', '/cur', '/sem', '/sta']
         headteachers = soup.find_all('a', class_='menu__link', href=lambda x: x and any(x.startswith(p) for p in
                                                                                         all_lastnames_headteachers))
         if headteachers:
             for FCs_headteacher in headteachers:
                 logging.info(f'{FCs_headteacher.get_text(strip=True)}')
-    _headteachers_()
+    find_headteachers()
 
     # Новости
     logging.info("\n Ссылки на новости:")
     news_links = set()
 
-    def last_news_links():
+    def find_last_news_links():
         for block in soup.find_all(class_=re.compile('news', re.IGNORECASE)):
             for news in block.find_all('a', href=True):
                 link = news['href']
@@ -84,7 +84,7 @@ def parse_gymnasium_19(url: str) -> None:
                 logging.info(f"{link}")
         else:
             logging.info("Не найдено.")
-    last_news_links()
+    find_last_news_links()
 
 
 if __name__ == "__main__":
